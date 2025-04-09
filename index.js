9abfc92d625d4ff6384fb19ed9db3d09fb098fed
vyas2610 <deepakvyas261092@gmail.com>
2025-04-09 17:52:06 +0530

    MapMethod

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-const arrData =[1,21,3,4,5,6,7,87];
+// const arrData = [1, 21, 3, 4, 5, 6, 7, 87];
 
+// let data = arrData.map((arr) => {
+//   return arr + 5;
+// });
+
+// console.log(data);
+
+const ApiData = [
+  {
+    id: 1,
+    name: "Book Name",
+    author: "Deeapk Vyas",
+  },
+  {
+    id: 2,
+    name: "Book Name",
+    author: "Deep",
+  },
+];
+
+const result = ApiData.map((data) => {
+  return data;
+});
+
+console.log(result);
